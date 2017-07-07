9655af58ba5825101578b027b1bcaa25bfe7e4f0
mambot <richard_bollet@hotmail.fr>
2017-07-07 16:00:42 +0200

    Init VueJs

---
+++ frontend/src/src/components/Welcome.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="welcome">
+    <img src="../assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <router-link to="/">Welcome</router-link><br />
+    <router-link to="/dashboard">Dashboard</router-link><br />
+    <router-link to="/settings">Settings</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'welcome',
+  data () {
+    return {
+      msg: 'Welcome to your Smart Speech Assistant'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
