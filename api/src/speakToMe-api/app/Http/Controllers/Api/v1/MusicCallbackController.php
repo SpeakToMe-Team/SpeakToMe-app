346a7df51b3d2e08a9ba7307a8554433671501af
Nicolas <nicolas.aslantas@gmail.com>
2017-08-23 15:58:16 +0200

    [updated] music api controller

---
+++ api/src/speakToMe-api/app/Http/Controllers/Api/v1/MusicCallbackController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Api\v1;
+
+use GuzzleHttp\Client;
+
+class MusicCallbackController
+{
+
+    public function index()
+    {
+        return "Hello World";
+    }
+}
