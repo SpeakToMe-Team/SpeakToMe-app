4a81ce4f3b4a7b958cd7c7db1286c72852386fe1
mambot <richard_bollet@hotmail.fr>
2017-08-29 15:54:15 +0200

    update dashboard

--- api/src/speakToMe-api/routes/api.php
+++ api/src/speakToMe-api/routes/api.php
@@ -6,15 +6,15 @@ use Dingo\Api\Routing\Router;
 $api = app(Router::class);
 
 /*ROUTE AVEC MOT DE PASSE PROD*/
-$api->version('v1', [], function (Router $api) {
+/*$api->version('v1', [], function (Router $api) {
 
     $api->get('speech', 'App\Http\Controllers\Api\v1\SpeechController@index')->middleware('CheckRequest')->name('speech');
 
-});
+});*/
 
 /*DEBUG SANS MOT DE PASSE */
-/*$api->version('v1', [], function (Router $api) {
+$api->version('v1', [], function (Router $api) {
 
     $api->get('speech', 'App\Http\Controllers\Api\v1\SpeechController@index')->name('speech');
 
-});*/
+});
